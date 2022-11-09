2a8080d0ef6c36674afa032e6b1673572aeadb9e
priscand <117074829+priscand@users.noreply.github.com>
2022-11-09 16:40:37 +0100

    Add files via upload

---
+++ exo-Le taxi/taxi.js
@@ -0,0 +1,44 @@
+let person = {
+    name :'john'
+    cool : 10,
+}
+//pas de class comme il ya qu'une personne
+
+
+const musics = [
+    "Wejdene - Anissa",
+    "beyonce - halo",
+    "beyonce - who run the word",
+    "beyonce - cuff it",
+    "beyonce - crazy il love"
+]
+
+
+
+let trajet = {
+    change :0,
+    redlights:30,
+}
+
+isMusic(){
+    return Math.floor(Math.random()*(5-0+1))+0;  
+}
+
+function takeTaxi (person,trajet){
+    while(trajet.redlights > 0 ){
+        radio.change(musics)
+        if(musics <= 0){
+            console.log(musics + '')
+        }
+
+
+    }
+
+
+
+
+
+
+}
+
+
---
+++ exo-tueur en série/tueur.js
@@ -0,0 +1,60 @@
+let person = {
+    name :'jason'
+    pdv : 100,
+}
+
+class perso {
+    constructor (cara,name,health,attack,luck){
+        this.cara = cara
+        this.name = name
+        this.health = health
+        this.attack = attack
+        this.luck = luck
+    }
+}
+
+class survivor{
+    constructor(name,carac){
+        this.name = name
+        this.carac= carac
+    }
+}
+
+
+const cract =[
+    'gros',
+    'mince',
+    'petit',
+    'grand',
+    'nerd',
+
+]
+
+
+
+const names = [
+    'aa',
+    'bb',
+    'cc',
+    'dd',
+    'ee',
+]
+
+let survivor = 
+
+is(){
+    return Math.floor(Math.random()*())+0;  
+}
+
+
+
+attackpersonne(names){
+    if(names.isLucky()){
+        console.log(survivant.name + ' à infligé 15 points de dégats et meurt')
+    } else{
+        survivant.pdv -= (this.attack - pokemon.defense)
+    }
+}
+
+
+
